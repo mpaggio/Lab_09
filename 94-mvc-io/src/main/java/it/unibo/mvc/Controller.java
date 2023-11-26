7e2288716ee8534b406178fa82cd7bc78ee08207
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-11-26 18:11:12 +0100

    Defined Controller interface

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,17 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    public void setNextStringToPrint(String string);
+
+    public String getNextStringToPrint();
+    
+    public List<String> getHistoryOfPrintedString();
+
+    public void printString();
 }
