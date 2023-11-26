5955255e4dc800a6c2a35afc5ba0283fb671ac36
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-11-26 18:17:59 +0100

    Created Controller interface implementation

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+    private List<String> history = new LinkedList<>();
+    private String nextString;
+
+    @Override
+    public void setNextStringToPrint(String string) {
+        if(string != null){
+            this.nextString = string;
+        }
+    }
+
+    @Override
+    public String getNextStringToPrint() {
+        return this.nextString;
+    }
+
+    @Override
+    public List<String> getHistoryOfPrintedString() {
+        return this.history;
+    }
+
+    @Override
+    public void printString() {
+        if(this.nextString == null){
+            throw new IllegalStateException("String is not set yet");
+        }
+        this.history.add(this.nextString);
+        System.out.println(this.nextString);
+    }
 
 }
